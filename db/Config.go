471c9b538a3af18b658b2717cce47d537d450b75
Asif Mohammad Mollah <admin@mrasif.in>
2020-05-08 00:05:21 +0530

    build: procfile added

--- db/Config.go
+++ db/Config.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"os"
+
 	"github.com/astaxie/beego/orm"
 )
 
@@ -11,25 +12,30 @@ var db orm.Ormer
 // GetDb : It will returns orm object
 func GetDb() orm.Ormer {
 	if db == nil {
+		dbHost := os.Getenv("DB_HOST")
 		dbName := os.Getenv("DB_NAME")
 		dbUser := os.Getenv("DB_USERNAME")
 		dbPassword := os.Getenv("DB_PASSWORD")
-		
+
+		if dbHost == "" {
+			fmt.Println("Environment variable DB_HOST is null.")
+			return nil
+		}
 		if dbName == "" {
 			fmt.Println("Environment variable DB_NAME is null.")
-			return nil;
+			return nil
 		}
 		if dbUser == "" {
 			fmt.Println("Environment variable DB_USERNAME is null.")
-			return nil;
+			return nil
 		}
 		if dbPassword == "" {
 			fmt.Println("Environment variable DB_PASSWORD is null.")
-			return nil;
+			return nil
 		}
-		
+
 		orm.RegisterDriver("postgres", orm.DRPostgres)
-		orm.RegisterDataBase("default", "postgres", "postgres://"+dbUser+":"+dbPassword+"@localhost:5432/"+dbName+"?sslmode=disable")
+		orm.RegisterDataBase("default", "postgres", "postgres://"+dbUser+":"+dbPassword+"@"+dbHost+":5432/"+dbName+"?sslmode=disable")
 
 		// This is for auto generating tables
 		orm.RunSyncdb("default", false, true)
